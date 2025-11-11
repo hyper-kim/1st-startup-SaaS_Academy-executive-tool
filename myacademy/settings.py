ca3e505d82d51563e190dc5709a1dbc3700b0296
hyper-kim <kjy6624kss@gmail.com>
2025-11-11 14:20:45 +0000

    db 수정

--- myacademy/settings.py
+++ myacademy/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'myacademy.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # 여기를 수정
-        'NAME': 'myacademy_db',        # MySQL에서 만든 DB 이름
+        'NAME': 'railway',        # MySQL에서 만든 DB 이름
         'USER': 'root',                 # MySQL 사용자 이름
-        'PASSWORD': 'hyperkjy1405!',    # MySQL 비밀번호
-        'HOST': 'localhost',
-        'PORT': '3306',                 # MySQL 기본 포트
+        'PASSWORD': 'oDtDZgPgLBzPCOXWMiHmIIZpbZSwsntM',    # MySQL 비밀번호
+        'HOST': 'shortline.proxy.rlwy.net',
+        'PORT': '28342',                 # MySQL 기본 포트
     }
 }
 
